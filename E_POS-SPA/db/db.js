2a5084770f9b248bbcfb75d45e131b927ed15ca9
Sandun-Induranga <sandutharu40@gmail.com>
2022-10-21 21:24:23 +0530

    First Commit

--- E_POS-SPA/db/db.js
+++
@@ -1,9 +0,0 @@
-/**
- * @author : Sanu Vithanage
- * @since : 0.1.0
- **/
-
-var customers = [];
-var items = [];
-var orders = [];
-
--- E_POS-SPA/model/Customer.js
+++
@@ -1,8 +0,0 @@
-/**
- * @author : Sanu Vithanage
- * @since : 0.1.0
- **/
-
-var customer = {
-    id: '', name: '', address: '', salary: 0
-}
