e2aeadb4ba10c77fbefd94691b94a325143f6871
Sandun-Induranga <sandutharu40@gmail.com>
2022-10-21 23:23:15 +0530

    Content Updated

--- A_Basics/assets/js/my-script.js
+++ A_Basics/assets/js/my-script.js
@@ -351,4 +351,79 @@ console.log("Hello there I'm from the console")
 *   student.id;
 *   student.id = "S002";
 *
+*   Can add object properties dynamically
+*
+*             var customer = {
+*               id:'C001'
+*           };
+*
+*           customer.name = "Dasun"
+*           delete customer.id
+*
+*   --- JS Property Descriptors ---
+*
+*       A Mechanism that use to change the behaviour of a object
+*
+*           writable
+*           enumerable - Property visibility closed
+*           configurable - If this changed to false, can't change any property descriptor again
+*
+*       - writable
+*
+*
+*       Object.defineProperty(customer, 'id',{
+*               writable: false
+*       });
+*
+*       Object.getOwnPropertyDescriptor(customer,"id");
+*
+*   There are two type of properties
+*       1. Data properties
+*               If value is directly assigned
+*
+*                   value : true
+*                   writable: true
+*                   configurable: true
+*                   enumerable: true
+*
+*           var c = {
+*               id: "C001"  // Data Property
+*           }
+*       2. Accessor properties
+*               If value is directly not assigned
+*
+*                   configurable: false;
+*                   enumerable: false;
+*                   set
+*                   get
+*
+*                   // line 60
+*
+* * ************************************* Functional Base Object ****************************************
+*
+*   function Customer(){
+*       this.id = 'C001'
+*   }
+*
+*   var customer = new Customer();
+*
+*
+*  function Customer() {
+*     this.id = "C001"
+* }
+*
+* var customer = new Customer();
+* console.log(customer)
+*
+*   function Car(make,year) {
+*       this.make = make;
+*       this.year = year;
+*       this.park = function park(){
+*           console.log(`${this.make} - ${this.year} Parked`)
+*       }
+*   }
+*
+* var car1 = new Car("BMW","2022");
+* console.log(car1);
+*
 *  */
